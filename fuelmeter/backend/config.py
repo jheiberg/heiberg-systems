fbc0279a62da77ac6f6b38a5bce32cc81240f294
Jako Heiberg <jako@heiberg.co.za>
2024-05-31 14:13:55 +0200

    Changes

--- fuelmeter/backend/config.py
+++ fuelmeter/backend/config.py
@@ -2,6 +2,6 @@
 class CoolConfig(object):
     
     MY_ENV_VAR = "XY"
-    #SQLALCHEMY_DATABASE_URI = "postgresql://heiberg:heiberg@db:5432/fuelmeter"
-    SQLALCHEMY_DATABASE_URI = "postgresql://heiberg:heiberg@localhost:5432/fuelmeter"
+    SQLALCHEMY_DATABASE_URI = "postgresql://heiberg:heiberg@db:5432/fuelmeter"
+    #SQLALCHEMY_DATABASE_URI = "postgresql://heiberg:heiberg@localhost:5432/fuelmeter"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
