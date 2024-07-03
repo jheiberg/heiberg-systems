8c321bf550a686be1aca997059acaf471be894bf
Jako Heiberg <jako@heiberg.co.za>
2024-07-03 15:07:02 +0200

    Adding frontend to the mix

--- fuelmeter/frontend/src/App.js
+++ fuelmeter/frontend/src/App.js
@@ -1,15 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import keycloak from './keycloak';
+import axios from 'axios';
+import querystring from 'querystring';
 
 const App = () => {
     const [authenticated, setAuthenticated] = useState(false);
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
-        keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
-            setAuthenticated(authenticated);
-        }).catch(error => {
-            console.error('Failed to initialize Keycloak', error);
-        });
+        keycloak.init({ onLoad: 'login-required' }).then(auth => {
+            setAuthenticated(auth);
+
+            if (auth) {
+                keycloak.loadUserProfile().then(profile => {
+                    console.log('User profile', profile);
+                });
+
+                axios.post('http://localhost:8080/realms/heiberg/protocol/openid-connect/token',
+                    querystring.stringify({
+                        grant_type: 'client_credentials', //gave the values directly for testing
+                        client_id: 'flask-api',
+                        client_secret: 'zdejNttWWYZz8fbawDdc5Y02tHlJdg73'
+                    }), {
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    }
+                })
+                    .then(token => {
+                        axios.get('http://localhost:5000/fuel/logs', {
+                            headers: {
+                                'Authorization': `Bearer ${token.data.access_token}`
+                            }
+                        })
+                            .then(response => {
+                                setMessage(JSON.stringify(response.data));
+                            })
+                            .catch(error => {
+                                console.error('Failed to fetch protected resource', error);
+                            });
+                    })
+                    .catch(error => {
+                        console.error('Failed to fetch token', error);
+                    });
+            };
+        })
     }, []);
 
     if (!authenticated) {
@@ -19,6 +53,7 @@ const App = () => {
     return (
         <div className="App">
             <h1>Welcome to React</h1>
+            <div>{message}</div>
         </div>
     );
 }
